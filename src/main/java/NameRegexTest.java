e9f34286ea3d5e730de240a2fa3a8aca64cecefa
Ces <ces@Zipcoders-MacBook-Pro-42.local>
2023-12-11 14:08:06 -0500

    UpdatedName test

---
+++ src/main/java/NameRegexTest.java
@@ -0,0 +1,33 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.Comparator;
+
+public class NameRegexTest {
+
+    @Test
+    public void testNameMethod() {
+        //Given
+        String input = "Name";
+
+        //When
+        Comparator<String> matches = input.CASE_INSENSITIVE_ORDER;
+        // int occurrences = ?
+        //Then
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testPriceMethod() {
+        //Given
+        String input = "Price";
+
+        //When
+        Comparator<String> matches = input.CASE_INSENSITIVE_ORDER;
+        // int occurrences = ?
+        //Then
+        Assert.assertTrue(true);
+    }
+
+
+}
