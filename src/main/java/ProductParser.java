d0e2a5215f7fd2a725be288dd60ee03fee383bcd
Ces <ces@Zipcoders-MacBook-Pro-42.local>
2023-12-11 17:13:50 -0500

    updated product fields test and classes

--- src/main/java/ProductParser.java
+++ src/main/java/ProductParser.java
@@ -1,7 +1,15 @@
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ProductParser {
     public static String extractNameFromProductLine(String input) {
+        String regexString = "([nN][aA][mM][eE]:)[a-zA-Z]+;";
+        Pattern pattern = Pattern.compile(regexString);
+        Matcher matcher = pattern.matcher(input);
+        for (int i = 0; matcher.find(); i++) {
+            return matcher.group();
+        }
         return null;
     }
 
