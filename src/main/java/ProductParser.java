b76fde11a469e533393fef1378d74a10e5bd6ce5
Ces <ces@Zipcoders-MacBook-Pro-42.local>
2023-12-13 10:56:46 -0500

    completed ProductParse

--- src/main/java/ProductParser.java
+++ src/main/java/ProductParser.java
@@ -4,24 +4,42 @@ import java.util.regex.Pattern;
 
 public class ProductParser {
     public static String extractNameFromProductLine(String input) {
-        String regexString = "([nN][aA][mM][eE]:)[a-zA-Z]+;";
+        String regexString = "[nN][aA][mM][eE]:([a-zA-Z]+)";
         Pattern pattern = Pattern.compile(regexString);
         Matcher matcher = pattern.matcher(input);
-        for (int i = 0; matcher.find(); i++) {
-            return matcher.group();
+        if (matcher.find()) {
+            return matcher.group(1);
         }
         return null;
     }
 
     public static String extractPriceFromProductLine(String input) {
+        String regexString = "[pP][rR][iI][cC][eE]:([0-9]\\d*\\.?\\d+)"; // search numbers 0-9\allows for additional number\adds decimal\allows for additonal number after decimal
+        Pattern pattern = Pattern.compile(regexString);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
         return null;
     }
 
     public static String extractTypeFromProductLine(String input) {
+        String regexString = "[tT][yY][pP][eE]:([a-zA-Z]+)";
+        Pattern pattern = Pattern.compile(regexString);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
         return null;
     }
 
     public static String extractExpirationFromProductLine(String input) {
+        String regexString = "[eE][xX][pP][iI][rR][aA][tT][iI][oO][nN]:([0-9]\\d*/[0-31]\\d*/\\d{4})"; // [0-9]\d* <- month /[0-31]\d* <- date \\d{4}<- matches the year(four digits)
+        Pattern pattern = Pattern.compile(regexString);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
         return null;
     }
 
