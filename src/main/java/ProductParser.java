0353399e6b619dc7c90f4d7df3610fcfe1e76559
hyunjialee <hyjialee@gmail.com>
2023-12-13 16:39:09 -0500

    formatting

--- src/main/java/ProductParser.java
+++ src/main/java/ProductParser.java
@@ -116,14 +116,47 @@ public class ProductParser {
 
             for (Map.Entry<String, Integer> innerEntry : innerMap.entrySet()) {
                 if(!innerEntry.getKey().contains(".") && !innerEntry.getKey().equals("Error")) {
-                    result.append("   \n" + "Name:   " + innerEntry.getKey().substring(0,1).toUpperCase() + innerEntry.getKey().substring(1) +  "    \t " + "seen: " + innerEntry.getValue() + " times" + "\n=============" + "\t\t ============");
+                    result.append("   \n" + "name:   " + innerEntry.getKey().substring(0,1).toUpperCase()
+                            + innerEntry.getKey().substring(1)
+                            +  "\t\t" + "seen: "
+                            + innerEntry.getValue()
+                            + " times" + "\n============="
+                            + "\t\t=============");
                 } else if (innerEntry.getKey().contains(".")) {
-                    result.append("   \n" + "Price:   " + innerEntry.getKey() + "    \t " + "seen: " + innerEntry.getValue() + " times" + "\n-------------" + "\t\t -------------");
+                    result.append("   \n" + "Price:   " + innerEntry.getKey()
+                            + "    \t" + "seen: "
+                            + innerEntry.getValue()
+                            + " times" + "\n-------------"
+                            + "\t\t-------------");
                 } else {
-                    result.append("\n" + innerEntry.getKey() +  "                seen: " + innerEntry.getValue() + " times");
+                    result.append("\n" + innerEntry.getKey() +  "         \t \tseen: " + innerEntry.getValue() + " times");
                 }
             }
         }
         return result.toString();
     }
+
+    String x = "name:    Milk \t\t seen: 6 times\n" +
+            "============= \t \t =============\n" +
+            "Price: \t 3.23\t\t seen: 5 times\n" +
+            "-------------\t\t -------------\n" +
+            "Price:   1.23\t\t seen: 1 time\n" +
+            "\n" +
+            "name:   Bread\t\t seen: 6 times\n" +
+            "=============\t\t =============\n" +
+            "Price:   1.23\t\t seen: 6 times\n" +
+            "-------------\t\t -------------\n" +
+            "\n" +
+            "name: Cookies     \t seen: 8 times\n" +
+            "=============     \t =============\n" +
+            "Price:   2.25        seen: 8 times\n" +
+            "-------------        -------------\n" +
+            "\n" +
+            "name:  Apples     \t seen: 4 times\n" +
+            "=============     \t =============\n" +
+            "Price:   0.25     \t seen: 2 times\n" +
+            "-------------     \t -------------\n" +
+            "Price:   0.23  \t \t seen: 2 times\n" +
+            "\n" +
+            "Errors         \t \t seen: 4 times\n";
 }
